ba38cdd470167498221bff209d840a899c850703
sreecharanpola <sreecharanpola@gmail.com>
2023-07-10 09:42:00 -0400

    Properties

--- src/main/java/com/example/polls/PollsApplication.java
+++ src/main/java/com/example/polls/PollsApplication.java
@@ -1,11 +1,25 @@
 package com.example.polls;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+
+import java.util.TimeZone;
 
 @SpringBootApplication
-public class PollsApplication {
+@EntityScan(basePackageClasses = {
+		PollsApplication.class,
+		Jsr310JpaConverters.class
+})
 
+public class PollsApplication {
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PollsApplication.class, args);
 	}
